2f81c43006a1f29736f23d253595954ee8811c27
Carol0427 <carolraul27@gmail.com>
2023-10-05 23:29:26 -0400

    step 0

--- lab-insta-parse/AppDelegate.swift
+++ lab-insta-parse/AppDelegate.swift
@@ -15,7 +15,7 @@ class AppDelegate: UIResponder, UIApplicationDelegate {
 
     func application(_ application: UIApplication, didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey: Any]?) -> Bool {
         // Override point for customization after application launch.
-
+//init commit
         // TODO: Pt 1 - Initialize Parse SDK
 
         // Add the following values from your Parse server.
